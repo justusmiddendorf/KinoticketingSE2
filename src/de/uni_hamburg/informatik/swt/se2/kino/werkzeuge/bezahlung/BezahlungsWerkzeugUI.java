258701f05a278559fb6f5cee73f36d1c1e45ae4f
justusmiddendorf <justus@noxt.de>
2023-07-07 15:55:23 +0200

    Hinzufügen des Dialoges

--- src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/bezahlung/BezahlungsWerkzeugUI.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/bezahlung/BezahlungsWerkzeugUI.java
@@ -1,5 +1,7 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.bezahlung;
 
+import javax.swing.JDialog;
+
 /**
  * * Die GUI des {@link BezahlungsWerkzeug}.
  * 
@@ -7,4 +9,27 @@ package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.bezahlung;
  */
 public class BezahlungsWerkzeugUI {
 
+    private JDialog _modal;
+
+    public BezahlungsWerkzeugUI() {
+        _modal = erstelleDialog();
+    }
+
+    /**
+     * Erstellt den Dialog fürs Bezahlen
+     * 
+     * @return JDialog der Dialog der angezeigt wird
+     */
+    private JDialog erstelleDialog() {
+        JDialog dialog = new JDialog();
+
+        return dialog;
+    }
+
+    /**
+     * Öffnen des Dialoges
+     */
+    public void oeffneDialog() {
+        _modal.setVisible(true);
+    }
 }
